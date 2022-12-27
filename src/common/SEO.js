2ea87407d1733e2ebbb7fa5f54f1b2b8cd3a12b6
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2022-12-27 16:25:21 -0600

    seo

--- src/common/SEO.js
+++ src/common/SEO.js
@@ -11,14 +11,7 @@ const SEO = ({ title }) => {
             <meta name="robots" content="noindex, follow" />
             <meta name="description" content="Jaizmora Digital Media hacemos realidad las ideas." />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <meta name="language" content="spanish" />
-            <meta name="rating" content="safe for kids" />
-
-
-            <meta property="og:url" content="http://www.jaizmora.com" />
-            <meta property="og:title" content="Jaizmora Digital Media" />
-            <meta property="og:description" content="Hacemos realidad las ideas." />
-            <meta property="og:image" content={Jaizmora} />
+           
         </>
     )
 }
